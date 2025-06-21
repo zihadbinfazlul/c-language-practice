c700abe2465ada8606f1acd96756a110b6c276f5
Zihad Bin Fazlul <zihadbinrozina@gmail.com>
2025-06-22 00:10:31 +0600

    area messure
    area measuring with c

---
+++ area.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int leanth,breadth;
+
+printf("the breadth is \n");
+scanf("%d", &breadth);
+
+printf("the leanth is \n");
+scanf(" %d",&leanth);
+
+printf("the area of the rectengular is %d",leanth*breadth);
+
+return 0;
+
+
+
+}
