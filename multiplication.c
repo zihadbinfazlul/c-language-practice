817a7bf3816d07a9564c5fd4a13b0c0fb62ddfc6
Zihad Bin Fazlul <zihadbinrozina@gmail.com>
2025-06-22 00:27:23 +0600

    multiplication

---
+++ multiplication.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int number,number2;
+printf("input the number:");
+scanf("%d", &number);
+printf("input the another number:");
+scanf("%d", &number2);
+printf("the multiply is:%d",number*number2);
+    return 0;
+}
